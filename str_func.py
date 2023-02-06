f40f091691d1f2fb6187596684e9a649bc4aa433
Natalia <detskina@list.ru>
2023-02-07 00:10:41 +0300

    add

--- str_func.py
+++ str_func.py
@@ -7,3 +7,9 @@ def func():
 def func_2():
   string = input()
   return string.title()
+
+''' принимает на вход строку и возвращает её с маленькими буквами каждого слова в строке'''
+def func_3():
+  string = input()
+  return string.lower()
+
